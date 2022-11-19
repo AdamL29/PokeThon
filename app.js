c469ff3dc5c1cf68a473614f00c1e1cd3a64f5a6
Adam <adamlederer@live.ca>
2022-11-19 14:54:37 -0700

    Finished index.html. Fixed selction button javascripts. Began working on math equations for battling.

--- app.js
+++ app.js
@@ -1,31 +1,48 @@
+
+function choiceHandler(event){
+    let parent = event.target.parentElement;
+    if(parent.id == "pokemon1"){
+        let pokemonJson = JSON.stringify(pokemon1);
+        Cookies.set(`pokemonSelection`, pokemonJson);
+        location.href = `battle.html`;
+    } else if(parent.id == "pokemon2"){
+        let pokemonJson = JSON.stringify(pokemon2);
+        Cookies.set(`pokemonSelection`, pokemonJson);
+        location.href = `battle.html`;
+    } else if(parent.id == "pokemon3"){
+        let pokemonJson = JSON.stringify(pokemon3);
+        Cookies.set (`pokemonSelection`, pokemonJson);
+        location.href = `battle.html`
+    }
+}
+
+
 let pokemon1 = {
     name: `Eevee`,
     img: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png`,
-    health: `200`,
+    health: 200,
     attackOptions: [`Tackle`, `Thunder`, `Flamethrower`, `Rest`]
-    // need to finish
 }
 
 let pokemon2 = {
     name: `Pikachu`,
     img: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png`,
+    health: 200,
     attackOptions: [`Tackle`, `Thunder`, `Flamethrower`, `Rest`]
-    // need to finish
 }
 
 let pokemon3 = {
     name: `Cyndaquil`,
     img: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/155.png`,
+    health: 200,
     attackOptions: [`Tackle`, `Thunder`, `Flamethrower`, `Rest`]
-    // need to finish
 }
 
-// not sure if correct yet & need to call function at end
 let selection1 = document.getElementById(`pokemon1`);
-selection1.querySelector(`button`).addEventListener(`click`, );
+selection1.querySelector(`button`).addEventListener(`click`, choiceHandler);
 
 let selection2 = document.getElementById(`pokemon2`);
-selection2.querySelector(`button`).addEventListener(`click`, );
+selection2.querySelector(`button`).addEventListener(`click`, choiceHandler);
 
 let selection3 = document.getElementById(`pokemon3`);
-selection3.querySelector(`button`).addEventListener(`click`, );
+selection3.querySelector(`button`).addEventListener(`click`, choiceHandler);
--- battle.js
+++ battle.js
@@ -1,7 +1,37 @@
+// attack buttons need to subtract health when clicked
+function tackleAttack(){
+// first, user clicks attack (need to Add Event Listener on page)
+// computer health decreased
+
+// if computer health <=0 then user wins
+// otherwise computer enemy attacks
+// user health decreases
+// if user health <=0 then computer wins
+// otherwise user clicks attack and REPEAT
+}
+
+// make functions for each attack? 
+function thunderAttack(){
+    
+}
+
+function flameAttack(){
+
+}
+
+let selectionJson = Cookies.get(`pokemonSelection`);
+let selection = JSON.parse(selectionJson);
+
+
 let enemyPoke = {
     name: `Mew`,
     img: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png`,
     health: `200`,
     attackOptions: [`Tackle`, `Psychic`, `Psybeam`, `Rest`]
-    // need to finish
 }
+
+document.getElementById(`charSelect`).insertAdjacentHTML(`afterbegin`, `<h2>${selection.name} </h2>`);
+document.getElementById(`charSelect`).insertAdjacentHTML(`afterbegin`, `<img src="${selection.img}" alt="Pokemone Eevee"</>`);
+document.getElementById(`charSelect`).insertAdjacentHTML(`afterbegin`, `<h3>${selection.health} </h3>`);
+
+// need to add event listener to the Attack buttons:
