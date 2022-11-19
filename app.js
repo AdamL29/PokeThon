d48169ec53f763e34a83c4f222882ef4b29a5307
Adam <adamlederer@live.ca>
2022-11-19 11:20:11 -0700

    Completed Index.html. Added more styling for positions. Begam JS creating of objects.

---
+++ app.js
@@ -0,0 +1,31 @@
+let pokemon1 = {
+    name: `Eevee`,
+    img: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png`,
+    health: `200`,
+    attackOptions: [`Tackle`, `Thunder`, `Flamethrower`, `Rest`]
+    // need to finish
+}
+
+let pokemon2 = {
+    name: `Pikachu`,
+    img: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png`,
+    attackOptions: [`Tackle`, `Thunder`, `Flamethrower`, `Rest`]
+    // need to finish
+}
+
+let pokemon3 = {
+    name: `Cyndaquil`,
+    img: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/155.png`,
+    attackOptions: [`Tackle`, `Thunder`, `Flamethrower`, `Rest`]
+    // need to finish
+}
+
+// not sure if correct yet & need to call function at end
+let selection1 = document.getElementById(`pokemon1`);
+selection1.querySelector(`button`).addEventListener(`click`, );
+
+let selection2 = document.getElementById(`pokemon2`);
+selection2.querySelector(`button`).addEventListener(`click`, );
+
+let selection3 = document.getElementById(`pokemon3`);
+selection3.querySelector(`button`).addEventListener(`click`, );
---
+++ battle.js
@@ -0,0 +1,7 @@
+let enemyPoke = {
+    name: `Eevee`,
+    img: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png`,
+    health: `200`,
+    attackOptions: [`Tackle`, `Psychic`, `Psybeam`, `Rest`]
+    // need to finish
+}
